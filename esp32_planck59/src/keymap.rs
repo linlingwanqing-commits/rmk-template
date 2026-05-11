a3b8cf2d014a9bf3991aea3e642b0643e9788912
linlingwanqing-commits <linlingwanqing@gmail.com>
2026-05-12 00:03:18 +0800

    lll

--- esp32_planck59/src/keymap.rs
+++ esp32_planck59/src/keymap.rs
@@ -2,7 +2,7 @@ use rmk::types::action::KeyAction;
 use rmk::{a, k, layer, mo, to, td};
 
 pub const ROW: usize = 5;
-pub const COL: usize =13;
+pub const COL: usize = 13;
 pub const NUM_LAYER: usize = 4;
 
 #[rustfmt::skip]
@@ -10,16 +10,16 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         // ========== Layer 0: 基础层 ==========
         layer!([
-            [k!(KcEsc), k!(Kc1), k!(Kc2), k!(Kc3), k!(Kc4), k!(Kc5), k!(Kc6), k!(Kc7), k!(Kc8), k!(Kc9), k!(Kc0), k!(KcBackslash), k!(KcBackspace)],
-            [k!(KcTab), k!(KcQ), k!(KcW), k!(KcE), k!(KcR), k!(KcT), k!(KcY), k!(KcU), k!(KcI), k!(KcO), k!(KcP), k!(KcLeftBracket), k!(KcRightBracket)],
-            [td!(0), k!(KcA), k!(KcS), k!(KcD), k!(KcF), k!(KcG), k!(KcH), k!(KcJ), k!(KcK), k!(KcL), k!(KcSemicolon), k!(KcQuote), td!(1)],
-            [k!(KcLeftShift), k!(KcZ), k!(KcX), k!(KcC), k!(KcV), k!(KcB), k!(KcN), k!(KcM), k!(KcComma), k!(KcDot), k!(KcUp), k!(KcSlash), td!(2)],
-            [k!(KcLeftCtrl), k!(KcLeftGui), k!(KcLeftAlt), k!(KcSpace), k!(KcSpace), mo!(1), a!(No), mo!(3), k!(KcSpace), k!(KcLeft), k!(KcDown), k!(KcRight), k!(KcRightCtrl)]
+            [k!(Escape), k!(Key1), k!(Key2), k!(Key3), k!(Key4), k!(Key5), k!(Key6), k!(Key7), k!(Key8), k!(Key9), k!(Key0), k!(Backslash), k!(Backspace)],
+            [k!(Tab), k!(KeyQ), k!(KeyW), k!(KeyE), k!(KeyR), k!(KeyT), k!(KeyY), k!(KeyU), k!(KeyI), k!(KeyO), k!(KeyP), k!(LeftBracket), k!(RightBracket)],
+            [td!(0), k!(KeyA), k!(KeyS), k!(KeyD), k!(KeyF), k!(KeyG), k!(KeyH), k!(KeyJ), k!(KeyK), k!(KeyL), k!(Semicolon), k!(Quote), td!(1)],
+            [k!(LeftShift), k!(KeyZ), k!(KeyX), k!(KeyC), k!(KeyV), k!(KeyB), k!(KeyN), k!(KeyM), k!(Comma), k!(Period), k!(UpArrow), k!(Slash), td!(2)],
+            [k!(LeftControl), k!(LeftGui), k!(LeftAlt), k!(Space), k!(Space), mo!(1), a!(No), mo!(3), k!(Space), k!(LeftArrow), k!(DownArrow), k!(RightArrow), k!(RightControl)]
         ]),
 
         // ========== Layer 1: Fn层 (MO1) ==========
         layer!([
-            [k!(KcGrave), k!(KcF1), k!(KcF2), k!(KcF3), k!(KcF4), k!(KcF5), k!(KcF6), k!(KcF7), k!(KcF8), k!(KcF9), k!(KcF10), k!(KcF11), k!(KcF12)],
+            [k!(Grave), k!(F1), k!(F2), k!(F3), k!(F4), k!(F5), k!(F6), k!(F7), k!(F8), k!(F9), k!(F10), k!(F11), k!(F12)],
             [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
             [to!(3), a!(No), a!(No), to!(0), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
             [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
@@ -28,20 +28,20 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
 
         // ========== Layer 2: Stop/PrtSc层 ==========
         layer!([
-            [k!(KcStop), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), k!(KcPrintScreen)],
+            [k!(Stop), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), k!(PrintScreen)],
             [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
             [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
             [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
-            [k!(KcHome), k!(KcEnd), k!(KcPageUp), k!(KcPageDown), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)]
+            [k!(Home), k!(End), k!(PageUp), k!(PageDown), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)]
         ]),
 
         // ========== Layer 3: 数字小键盘层 ==========
         layer!([
-            [k!(KcEsc), k!(KcKpMinus), k!(KcKpPlus), k!(KcKpSlash), k!(KcKp1), k!(KcKp2), k!(KcKp3), k!(KcKp4), k!(KcKp5), k!(KcKp6), k!(KcKp7), k!(KcKp8), k!(KcBackspace)],
-            [k!(KcKp7), k!(KcKp8), k!(KcKp9), k!(KcTab), k!(KcQ), k!(KcW), k!(KcE), k!(KcR), k!(KcT), k!(KcY), k!(KcU), k!(KcI), k!(KcO)],
-            [k!(KcKp4), k!(KcKp5), k!(KcKp6), k!(KcKpAsterisk), k!(KcA), k!(KcS), k!(KcD), k!(KcF), k!(KcG), k!(KcH), k!(KcJ), k!(KcK), k!(KcL)],
-            [k!(KcKp1), k!(KcKp2), k!(KcKp3), k!(KcLeftShift), k!(KcZ), k!(KcX), k!(KcC), k!(KcV), k!(KcB), k!(KcN), k!(KcM), k!(KcP), k!(KcBackslash)],
-            [k!(KcBackspace), k!(KcKpDot), k!(KcKp0), k!(KcLeftCtrl), k!(KcLeftGui), mo!(1), a!(No), k!(KcLeftAlt), k!(KcSpace), k!(KcLeft), k!(KcDown), k!(KcUp), k!(KcRight)]
+            [k!(Escape), k!(KpMinus), k!(KpPlus), k!(KpSlash), k!(Kp1), k!(Kp2), k!(Kp3), k!(Kp4), k!(Kp5), k!(Kp6), k!(Kp7), k!(Kp8), k!(Backspace)],
+            [k!(Kp7), k!(Kp8), k!(Kp9), k!(Tab), k!(KeyQ), k!(KeyW), k!(KeyE), k!(KeyR), k!(KeyT), k!(KeyY), k!(KeyU), k!(KeyI), k!(KeyO)],
+            [k!(Kp4), k!(Kp5), k!(Kp6), k!(KpAsterisk), k!(KeyA), k!(KeyS), k!(KeyD), k!(KeyF), k!(KeyG), k!(KeyH), k!(KeyJ), k!(KeyK), k!(KeyL)],
+            [k!(Kp1), k!(Kp2), k!(Kp3), k!(LeftShift), k!(KeyZ), k!(KeyX), k!(KeyC), k!(KeyV), k!(KeyB), k!(KeyN), k!(KeyM), k!(KeyP), k!(Backslash)],
+            [k!(Backspace), k!(KpDot), k!(Kp0), k!(LeftControl), k!(LeftGui), mo!(1), a!(No), k!(LeftAlt), k!(Space), k!(LeftArrow), k!(DownArrow), k!(UpArrow), k!(RightArrow)]
         ]),
     ]
 }
--- esp32_planck59/src/main.rs
+++ esp32_planck59/src/main.rs
@@ -67,32 +67,11 @@ async fn main(_s: Spawner) {
     let flash = async_flash_wrapper(flash);
 
     // Initialize the IO pins
-   let (row_pins, col_pins) = config_matrix_pins_esp!(
-    peripherals: peripherals,
-    input: [GPIO4, GPIO5, GPIO6, GPIO7, GPIO15],   // 行
-    output: [GPIO16, GPIO17, GPIO18, GPIO8, GPIO9, GPIO10, GPIO11, GPIO12, GPIO13, GPIO14, GPIO21, GPIO2, GPIO1]  // 列
-);
-
-   use rmk::behavior::morse::{Morse, MorseAction};
-
-   fn custom_morse_config() -> Vec<Morse> {
-    vec![
-        Morse::new(&[
-            MorseAction::Tap(k!(KcEnter)),
-            MorseAction::Hold(mo!(2)),
-            MorseAction::HoldAfterTap(k!(KcCapsLock)),
-        ]),
-        Morse::new(&[
-            MorseAction::Tap(k!(KcEnter)),
-            MorseAction::Hold(mo!(2)),
-            MorseAction::HoldAfterTap(k!(KcNumLock)),
-        ]),
-        Morse::new(&[
-            MorseAction::Tap(k!(KcDelete)),
-            MorseAction::Hold(k!(KcRightShift)),
-        ]),
-    ]
-}
+    let (row_pins, col_pins) = config_matrix_pins_esp!(
+        peripherals: peripherals,
+        input: [GPIO4, GPIO5, GPIO6, GPIO7, GPIO15],   // 行
+        output: [GPIO16, GPIO17, GPIO18, GPIO8, GPIO9, GPIO10, GPIO11, GPIO12, GPIO13, GPIO14, GPIO21, GPIO2, GPIO1]  // 列
+    );
 
     // RMK config
     let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, &[(0, 0), (1, 1)]);
